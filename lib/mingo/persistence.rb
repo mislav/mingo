1d42259b0f52c2103a188c8bc7fbb2b425aef6cb
Mislav Marohnić <mislav.marohnic@gmail.com>
2012-01-29 21:21:58 +0100

    get rid of InstanceMethods module

--- lib/mingo/persistence.rb
+++ lib/mingo/persistence.rb
@@ -3,7 +3,7 @@ require 'active_support/concern'
 class Mingo
   module Persistence
     extend ActiveSupport::Concern
-    
+
     module ClassMethods
       def create(obj = nil)
         new(obj).tap do |object|
@@ -12,52 +12,50 @@ class Mingo
         end
       end
     end
-    
-    module InstanceMethods
-      def initialize(*args)
-        @destroyed = false
-        super
-      end
 
-      def persisted?
-        !!id
-      end
+    def initialize(*args)
+      @destroyed = false
+      super
+    end
 
-      def save(options = {})
-        if persisted?
-          hash = values_for_update
-          unless %w[$set $unset] == hash.keys && hash.values.all? { |v| v.empty? }
-            update(hash, options)
-          end
-        else
-          self['_id'] = self.class.collection.insert(self.to_hash, options)
+    def persisted?
+      !!id
+    end
+
+    def save(options = {})
+      if persisted?
+        hash = values_for_update
+        unless %w[$set $unset] == hash.keys && hash.values.all? { |v| v.empty? }
+          update(hash, options)
         end
+      else
+        self['_id'] = self.class.collection.insert(self.to_hash, options)
       end
+    end
 
-      def update(doc, options = {})
-        self.class.collection.update({'_id' => self.id}, doc, options)
-      end
+    def update(doc, options = {})
+      self.class.collection.update({'_id' => self.id}, doc, options)
+    end
 
-      def reload
-        doc = self.class.first(id, :transformer => nil)
-        replace doc
-      end
+    def reload
+      doc = self.class.first(id, :transformer => nil)
+      replace doc
+    end
 
-      def destroy
-        self.class.collection.remove('_id' => self.id)
-        @destroyed = true
-        self.freeze
-      end
+    def destroy
+      self.class.collection.remove('_id' => self.id)
+      @destroyed = true
+      self.freeze
+    end
 
-      def destroyed?
-        @destroyed
-      end
-      
-      private
+    def destroyed?
+      @destroyed
+    end
 
-      def values_for_update
-        self.to_hash
-      end
+    private
+
+    def values_for_update
+      self.to_hash
     end
   end
 end
